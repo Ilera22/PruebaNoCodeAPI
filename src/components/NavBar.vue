b602a20c2545c712a5ef7777a69d2560c97689d6
Ilera22 <ilera.none@gmail.com>
2023-09-27 22:10:41 -0500

    Cambio en navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,89 +1,43 @@
 <template>
-    <div id="app">
-      <v-layout row justify-center>
-        <v-responsive v-if="$vuetify.breakpoint.smAndDown">
-          <v-app-bar app color="purple draken-4">
-            <v-btn plain @click="goToIndex">GremioKTG</v-btn>
-            <v-spacer></v-spacer>
-            <v-app-bar-nav-icon @click="menuOpen = !menuOpen"></v-app-bar-nav-icon>
-            <v-menu v-model="menuOpen" offset-y>
-              <v-list>
-                <v-list-item
-      
-                  v-for="item in nav"
-                  :key="item.icon"
-                  :to="item.to"
-                  :title="item.title"
-                  @click="handleMenuClick(item.to)"
-                >
-                  <v-list-item-title>{{ item.text }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </v-app-bar>
-        </v-responsive>
-  
-        <v-responsive v-else>
-          <v-app-bar app dark color="purple darken-4">
-            <v-btn plain @click="goToIndex">GremioKTG</v-btn>
-            <v-spacer></v-spacer>
-            <v-app-bar-items>
-              <v-btn
-                plain
-                v-for="item in nav"
-                :key="item.icon"
-                :to="item.to"
-                :title="item.title"
-              >
-                {{ item.text }}
-              </v-btn>
-            </v-app-bar-items>
-          </v-app-bar>
-        </v-responsive>
-      </v-layout>
-    </div>
-  </template>
+<v-app-bar
+        color="teal-darken-4"
+      >
+        <template v-slot:image>
+          <v-img
+            gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
+
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>Title</v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+  </v-app-bar>
+</template>
+
   
   <script>
   export default {
     name: "NavBar",
     data() {
       return {
-        dialog: false,
-        menuOpen: false,
-        nav: [
-          {
-            icon: "info",
-            text: "About",
-            title: "About this demo",
-            active: false,
-            to: "/about",
-          },
-          {
-            icon: "assignment_turned_in",
-            text: "Test",
-            title: "Some stuff that needs doing",
-            active: false,
-            to: "/Test",
-          },
-          {
-            icon: "email",
-            text: "Contact",
-            title: "Our Contact info",
-            active: false,
-            to: "/contact",
-          },
-        ],
+       
       };
-    },
-    methods: {
-      handleMenuClick(to) {
-        this.$router.push(to);
-        this.menuOpen = false; // Cerramos el menú después de la redirección
-      },
-      goToIndex() {
-        this.$router.push('/');
-      },
     },
   };
   </script>
