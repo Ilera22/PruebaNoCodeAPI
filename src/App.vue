14e8171a7f4e387cc94331997146d80bafa59a93
Ilera22 <ilera.none@gmail.com>
2023-09-28 13:24:52 -0500

    terminado de entender como funciona los themes

--- src/App.vue
+++ src/App.vue
@@ -27,5 +27,9 @@ import NavBar from './components/NavBar.vue'
 </template>
 
 <style scoped>
+.v-application {
+  background-color: var(--v-theme-background); /* Utiliza la variable CSS del tema de Vuetify */
+}
+
 
 </style>
