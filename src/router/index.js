e1487a4fdf2f10cd0f170374298e66fba5ca5045
Ilera22 <ilera.none@gmail.com>
2023-10-03 16:33:32 -0500

    correcion de rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import TestView from '../views/AboutView.vue'
+import TestView from '../views/TestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,11 +12,8 @@ const router = createRouter({
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      name: 'About',
+      component: () => import('@/views/AboutView.vue')
     },
     {
       path: '/test',
